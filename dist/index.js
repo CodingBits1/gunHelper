59d664bd22ef93c093d0c8cf82fc2e9f738bec7c
Renji <lprenji94@gmail.com>
2022-07-07 20:47:10 +0200

    added extra gunHelper features

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,29 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var _exportNames = {};
+var _exportNames = {
+  useGunAuth: true,
+  useGunValue: true
+};
 exports.default = void 0;
+Object.defineProperty(exports, "useGunAuth", {
+  enumerable: true,
+  get: function get() {
+    return _useGunAuth.default;
+  }
+});
+Object.defineProperty(exports, "useGunValue", {
+  enumerable: true,
+  get: function get() {
+    return _useGunValue.default;
+  }
+});
 
 var _gunHelper = _interopRequireDefault(require("./gun/gunHelper"));
+
+var _useGunAuth = _interopRequireDefault(require("./gun/hooks/useGunAuth"));
+
+var _useGunValue = _interopRequireDefault(require("./gun/hooks/useGunValue"));
 
 var _auth = require("./gun/auth");
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import gunHelper from "./gun/gunHelper";
+import useGunAuth from "./gun/hooks/useGunAuth";
+import useGunValue from "./gun/hooks/useGunValue";
 
 export default gunHelper;
 export * from "./gun/auth"
+export { useGunAuth, useGunValue }
